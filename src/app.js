21cf6a22051bd28627639d1c1b9c7ff0184e2596
hannah-deakin <hannah.v.deakin@gmail.com>
2022-02-01 19:47:37 +0000

    Displayed the current temperature

--- src/app.js
+++ src/app.js
@@ -1,1 +1,23 @@
+function displayTemperature(response) {
+  console.log(response.data);
 
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.name;
+
+  let descriptionElement = document.querySelector("#description");
+  descriptionElement.innerHTML = response.data.weather[0].description;
+
+  let humidityElement = document.querySelector("#humidity");
+  humidityElement.innerHTML = response.data.main.humidity;
+
+  let windElement = document.querySelector("#wind");
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+}
+
+let apiKey = "315a9bcbf521af440df195ee85aa6e09";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${"London"}&appid=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(displayTemperature);
